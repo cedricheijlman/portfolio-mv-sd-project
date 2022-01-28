f5642b070e0bd8644d6eeab2a39b2c98b4190c2d
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-01-28 10:28:37 +0100

    updated

--- stephanievanwettum/index.js
+++ stephanievanwettum/index.js
@@ -18,3 +18,24 @@ projectsShowBtn.addEventListener("click", () => {
   });
   bigProjects.classList.remove("none");
 });
+
+
+
+// Begin Scherm
+const homePage = document.querySelector(".homepage");
+
+// Tweede Pagina secties
+const infoPage = document.querySelectorAll(".fadeIn");
+
+// Verwijder begin screen FadeIn Second Screen
+removeScreen = () => {
+
+  // Verwijder Begin Scherm
+  homePage.style.display = "none"
+
+  // Display Tweede Scherm / Info Scherm
+  infoPage.forEach(section => {
+    section.classList.remove("fadeIn");
+    section.classList.add("fadeInAnimation")
+  })
+}
