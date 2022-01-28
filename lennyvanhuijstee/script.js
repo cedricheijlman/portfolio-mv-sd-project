670fbf5a9fbd4844731858a2ef5378fe94ffa1f9
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-01-28 10:07:08 +0100

    changed animation fade in

--- lennyvanhuijstee/script.js
+++ lennyvanhuijstee/script.js
@@ -4,22 +4,15 @@ const homePage = document.querySelector(".homepage");
 // Tweede Pagina secties
 const infoPage = document.querySelectorAll(".fadeIn");
 
-// fadeOut begin screen FadeIn Second Screen
+// Verwijder begin screen FadeIn Second Screen
 removeScreen = () => {
-    // Voeg fadeOut animatie aan begin scherm
-    homePage.classList.add('fadeOutAnimation')
 
-    // Na fadeOut animatie / 0.9secondes
-    setInterval(() => {
-        // Verwijder Begin Scherm
-        homePage.style.display = "none"
+    // Verwijder Begin Scherm
+    homePage.style.display = "none"
 
-        // Display Tweede Scherm / Info Scherm
-        infoPage.forEach(section => {
-            section.classList.remove("fadeIn");
-            section.classList.add("fadeInAnimation")
-        })
-
-    }, 900);
-
+    // Display Tweede Scherm / Info Scherm
+    infoPage.forEach(section => {
+        section.classList.remove("fadeIn");
+        section.classList.add("fadeInAnimation")
+    })
 }
