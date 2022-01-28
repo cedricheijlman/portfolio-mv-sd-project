53698694b99cc31b9870dc3579edffeb6bf87b7d
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-01-28 10:47:25 +0100

    Smooth transition

--- wesleyvanderploeg/script.js
+++ wesleyvanderploeg/script.js
@@ -1,6 +1,7 @@
 // Begin Scherm
 const homePage = document.querySelector(".homepage");
-
+const homePageUp = document.querySelector(".homepage__upper");
+const homePageDown = document.querySelector(".homepage__down")
 // Tweede Pagina secties
 const infoPage = document.querySelectorAll(".fadeIn");
 
@@ -8,7 +9,18 @@ const infoPage = document.querySelectorAll(".fadeIn");
 removeScreen = () => {
 
     // Verwijder Begin Scherm
-    homePage.style.display = "none"
+    homePage.style.width = "0"
+
+    // Smooth Transition
+    setInterval(() => {
+        homePage.style.height = "0"
+    }, 1000);
+
+    setInterval(() => {
+        homePageUp.style.display = "none"
+        homePageDown.style.display = "none"
+    }, 200);
+
 
     // Display Tweede Scherm / Info Scherm
     infoPage.forEach(section => {
