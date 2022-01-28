e89482bf745c67f9291c5b01f03343b64fbbbef6
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-01-28 11:13:15 +0100

    smooth transition

---
+++ cedricheijlman/script.js
@@ -0,0 +1,30 @@
+// Begin Scherm
+const homePage = document.querySelector(".homepage");
+const homePageUp = document.querySelector(".homepage__upper");
+const homePageDown = document.querySelector(".homepage__down");
+
+// Tweede Pagina secties
+const infoPage = document.querySelectorAll(".fadeIn");
+
+// verwijder begin screen FadeIn Second Screen
+removeScreen = () => {
+
+    // Verwijder Begin Scherm
+    homePage.style.width = "0"
+
+    // Smooth Transition
+    setInterval(() => {
+        homePageUp.style.display = "none"
+        homePageDown.style.display = "none"
+    }, 200);
+
+    setInterval(() => {
+        homePage.style.height = "0"
+    }, 1000);
+
+    // Display Tweede Scherm / Info Scherm
+    infoPage.forEach(section => {
+        section.classList.remove("fadeIn");
+        section.classList.add("fadeInAnimation")
+    })
+}
